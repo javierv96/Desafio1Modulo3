68d9756e1de87f52590981e46434612071881917
Javier Vergara <ja.vergarac96@gmail.com>
2024-02-10 13:57:11 -0300

    correcciones, documentacion e identado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,81 @@
+//Random opcion computadora
 const computadora = Math.floor(Math.random() * 3) + 1;
+
+//Ingreso de opcion Player 1
 const opcion = parseInt(prompt("Ingrese 1 para elegir Pierda, 2 para elegir Papel, 3 para elegir Tijera"));
-switch (opcion){
+console.log(opcion)
+
+//switch Player 1
+switch (opcion) {
+    //Piedra
     case 1:
-        if (computadora === 1){
+        // Piedra V/s Piedra
+        if (computadora === 1) {
             alert("Empate");
-        }else if (computadora === 2){
+        }
+        // Piedra V/s Papel
+        else if (computadora === 2) {
             alert("Lastima, Gana la3 Computadora");
-        }else if(computadora === 2){
+        }
+        // Piedra V/s Tijera
+        else if (computadora === 2) {
             alert("Felicidades, Ganaste!");
         }
         break;
+    //Papel
     case 2:
-        if (computadora === 1){
+        // Papel V/s Piedra
+        if (computadora === 1) {
             alert("Felicidades, Ganaste!");
-        }else if (computadora === 2){
+        }
+        // Papel V/s Papel
+        else if (computadora === 2) {
             alert("Empate");
-        }else if(computadora === 2){
+        }
+        // Papel V/s Tijera
+        else if (computadora === 2) {
             alert("Lastima, Gana la Computadora");
         }
         break;
+    //Tijera
     case 3:
-        if (computadora === 1){
+        // Tijera V/s Piedra
+        if (computadora === 1) {
             alert("Lastima, Gana la computadora");
-        }else if (computadora === 2){
+        }
+        // Tijera V/s Papel
+        else if (computadora === 2) {
             alert("Felicidades, Ganaste!");
-        }else if(computadora === 2){
+        }
+        // Tijera V/s Tijera
+        else if (computadora === 2) {
             alert("Empate");
         }
         break;
+    //En caso de elegir una opcion no valida.
     default:
-        alert("Ha ocurrido un error");
+        alert("Warning: Ha ocurrido un error, la opcion elegida no es valida.");
         break;
-    
+
 }
-switch (computadora){
+//switch Computadora
+console.log(computadora);
+switch (computadora) {
+    //Piedra
     case 1:
-        if (computadora === 1){
-            alert ("La computadora eligio la opcion: Piedra");
-        }
+        alert("La computadora eligio la opcion: Piedra");
+        break;
+    //Papel
     case 2:
-        if (computadora === 2){
-            alert ("La computadora eligio la opcion: Papel");
-        }
+        alert("La computadora eligio la opcion: Papel");
+        break;
+    //Tijera
     case 3:
-        if (computadora === 3){
-            alert ("La computadora eligio la opcion: Tijera");
-        }
+        alert("La computadora eligio la opcion: Tijera");
+        break;
+    //ERR0R
+    default:
+        alert("Ha ocurrido un error.");
+        break;
 }
 
